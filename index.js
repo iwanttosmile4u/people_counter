9d0dc81027dbfb8963f0ac21b43f63a4e157a33d
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2023-11-01 23:33:52 +0200

    practice with string and number data types

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ function increment() {
   resultOnPage.innerText = count;
 }
 
+function save() {
+  console.log(count);
+}
+
+// let userName = 'per';
+
+// let message = 'You have tree new notifications';
+// console.log(message);
+
+// let messageToUser = message + ', ' + userName + '!';
+// console.log(messageToUser);
+
+// const myName = 'Tania';
+// const greeting = 'Hi, my name is ';
+
+// const myGreeting = greeting + myName + '!';
+// console.log(myGreeting);
+
+let welcomEl = document.getElementById('welcome-el');
+
+const myName = 'Tania';
+const greeting = 'Welcome back ';
+
+welcomEl.innerText = greeting + myName;
